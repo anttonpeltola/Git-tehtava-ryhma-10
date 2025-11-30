fb75ccd8983aacae25a4062e103f5af76c89a4d8
Miko <miko.koskinen@student.hamk.fi>
2025-11-30 18:00:36 +0200

    Koristelua
    T: Kehittäjä 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Kehittäjä 1");
-        System.out.println("Kehittäjä 2 paikalla.");
+        System.out.println("\n+---------------------------+");
+        System.out.println("|   GIT-tehtävä-ryhmä-10    |");
+        System.out.println("+---------------------------+\n");
+        System.out.println("Tiimi:");
+        System.out.println("** Kehittäjä 1**");
+        System.out.println("** Kehittäjä 2**");
+        System.out.println("** SEURAAVA**");    //Kirjoita tähän oma nimesi
+
+        System.out.println("\n" + "=".repeat(20));
+        System.out.println("\n Aurinkoista syksyä");
+        System.out.println("\n" + "=".repeat(20));
+
     }
 }
