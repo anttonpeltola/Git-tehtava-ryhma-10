1f56cf078d1820c700a4668594ad9f365e8a40e7
Miko <miko.koskinen@student.hamk.fi>
2025-11-30 17:12:45 +0200

    Lisätty println
    Kehittäjä nro 2. liittynyt projektiin.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kehittäjä 1");
+        System.out.println("Kehittäjä 2 paikalla.");
     }
 }
